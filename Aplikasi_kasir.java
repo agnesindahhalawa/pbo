be3887e06eb5c5df101c45bc88e5f8dc3d4b675a
agnesindahhalawa <162179814+agnesindahhalawa@users.noreply.github.com>
2024-03-05 01:02:25 +0700

    Add files via upload

---
+++ Aplikasi_kasir.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package perhitungan_barang;
+
+/**
+ *
+ * @author AgnesHalawa
+ */
+public class Aplikasi_kasir {
+
+    public static void main(String[] args) {
+      new from_utama().show();
+        System.out.println("Hello World!");
+    }
+}
